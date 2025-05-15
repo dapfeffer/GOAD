6b8da9b087c589b27a2feb2e89089e86bca09f38
David Pfeffer <davidpfeffer@protonmail.ch>
2025-05-15 15:48:43 +0000

    add mitre caldera

--- scripts/setup_azure.sh
+++ scripts/setup_azure.sh
@@ -19,35 +19,6 @@ python3 -m pip install pywinrm
 sudo sed -i '/force_color_prompt=yes/s/^#//g' /home/*/.bashrc
 sudo sed -i '/force_color_prompt=yes/s/^#//g' /root/.bashrc
 
-# add guacamole based on autoinstaller
-# first download to bootstrap
-#wget https://raw.githubusercontent.com/itiligent/Guacamole-Install/main/1-setup.sh
-
-# then change config for our environment
-#sed -i 's/SERVER_NAME=""/SERVER_NAME="jumpbox"/g' 1-setup.sh
-#sed -i 's/LOCAL_DOMAIN=""/LOCAL_DOMAIN="local"/g' 1-setup.sh
-#sed -i 's/INSTALL_MYSQL=""/INSTALL_MYSQL="true"/g' 1-setup.sh
-#sed -i 's/SECURE_MYSQL=""/SECURE_MYSQL="true"/g' 1-setup.sh
-#sed -i 's/MYSQL_ROOT_PWD=""/MYSQL_ROOT_PWD="supersecretpassword"/g' 1-setup.sh
-#sed -i 's/GUAC_PWD=""/GUAC_PWD="evenmoresecretpassword"/g' 1-setup.sh
-#sed -i 's/INSTALL_TOPT=""/INSTALL_TOPT="false"/g' 1-setup.sh
-#sed -i 's/INSTALL_DUO=""/INSTALL_DUO="false"/g' 1-setup.sh
-#sed -i 's/INSTALL_LDAP=""/INSTALL_LDAP="false"/g' 1-setup.sh
-#sed -i 's/INSTALL_QCONNECT=""/INSTALL_QCONNECT="false"/g' 1-setup.sh
-#sed -i 's/INSTALL_HISTREC=""/INSTALL_HISTREC="false"/g' 1-setup.sh
-#sed -i 's/GUAC_URL_REDIR=""/GUAC_URL_REDIR="true"/g' 1-setup.sh
-#sed -i 's/INSTALL_NGINX=""/INSTALL_NGINX="true"/g' 1-setup.sh
-#sed -i 's/SELF_SIGN=""/SELF_SIGN="true"/g' 1-setup.sh
-
-# first we need to install nginx as the script is not able to do it
-#sudo apt-get install -y nginx
-
-# then we run the script
-#chmod +x 1-setup.sh
-#
-#./1-setup.sh
-
-
 # install docker
 # https://docs.docker.com/engine/install/ubuntu/
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
@@ -73,4 +44,8 @@ cd guacamole-docker-compose
 sudo ./prepare.sh
 sudo docker-compose up -d
 
-
+# add mitre caldera
+git clone https://github.com/mitre/caldera.git --recursive --branch 5.3.0
+cd caldera
+sudo docker build --build-arg WIN_BUILD=true . -t caldera:server
+docker run -d -p 7010:7010 -p 7011:7011/udp -p 7012:7012 -p 8888:8888 caldera:server
