3fb5e8973ae691577294cdd8d038e4a7019aa152
David Pfeffer <davidpfeffer@protonmail.ch>
2025-05-15 11:28:17 +0000

    add azure network security group as in PR https://github.com/Orange-Cyberdefense/GOAD/pull/342

--- template/provider/azure/network.tf
+++ template/provider/azure/network.tf
@@ -27,6 +27,31 @@ resource "azurerm_network_interface" "goad-vm-nic" {
   }
 }
 
+resource "azurerm_network_security_group" "nsg" {
+  name                 = "{{lab_name}}-subnet-nsg"
+  location             = azurerm_resource_group.resource_group.location
+  resource_group_name  = azurerm_resource_group.resource_group.name
+
+  security_rule {
+    name                          = "AllowSSHInboundOnly"
+    priority                      = 100
+    direction                     = "Inbound"
+    access                        = "Allow"
+    protocol                      = "Tcp"
+    source_port_range             = "*"
+    destination_port_range        = "22"
+    source_address_prefix         = "*"
+    destination_address_prefix    = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "nsg_association" {
+  subnet_id                       = azurerm_subnet.subnet.id
+  network_security_group_id       = azurerm_network_security_group.nsg.id
+}
+
+
+
 resource "azurerm_network_interface" "goad-linux-vm-nic" {
   for_each = var.linux_vm_config
 
