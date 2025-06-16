49a69ae011e9fa5b36e3f79436103add61abd1d9
David Pfeffer <davidpfeffer@protonmail.ch>
2025-06-16 10:08:00 +0000

    add 8443 firewall allow rule

--- template/provider/azure/network.tf
+++ template/provider/azure/network.tf
@@ -54,6 +54,17 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix         = "*"
     destination_address_prefix    = "*"
   }
+  security_rule {
+    name                          = "AllowAltHTTPSInbound"
+    priority                      = 110
+    direction                     = "Inbound"
+    access                        = "Allow"
+    protocol                      = "Tcp"
+    source_port_range             = "*"
+    destination_port_range        = "8443"
+    source_address_prefix         = "*"
+    destination_address_prefix    = "*"
+  }
 }
 
 resource "azurerm_subnet_network_security_group_association" "nsg_association" {
