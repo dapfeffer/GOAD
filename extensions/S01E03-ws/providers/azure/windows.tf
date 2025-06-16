3a6f8242f9acea61352049395bc7d41316d72dc5
dapfeffer <davidpfeffer@protonmail.ch>
2025-06-16 13:22:07 +0200

    Add extension for two workstations in s01e03

---
+++ extensions/S01E03-ws/providers/azure/windows.tf
@@ -0,0 +1,20 @@
+"ws01" = {
+  name               = "ws01"
+  publisher          = "MicrosoftWindowsDesktop"
+  offer              = "office-365"
+  windows_sku        = "win10-22h2-avd-m365"
+  windows_version    = "latest"
+  private_ip_address = "{{ip_range}}.31"
+  password           = "EP+xh7Rk6j90"
+  size               = "Standard_B2s"
+}
+"ws02" = {
+  name               = "ws02"
+  publisher          = "MicrosoftWindowsDesktop"
+  offer              = "office-365"
+  windows_sku        = "win10-22h2-avd-m365"
+  windows_version    = "latest"
+  private_ip_address = "{{ip_range}}.31"
+  password           = "9wioY9tG2+8"
+  size               = "Standard_B2s"
+}
